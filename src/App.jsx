f0af9969390c7d44eb835aeaf050e4988086c9aa
Akash Karan <akashkaran11@gmail.com>
2026-03-03 14:52:59 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from "react"
+import Hearder from "./components/header/Hearder"
+import Footer from "./components/footer/Footer"
+import Home from "./pages/Home"
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
